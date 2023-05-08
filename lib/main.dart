f621ea8931db278a3ce456bdd9934d432e29a6b1
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-05-08 13:47:24 +0700

    💄 🎨 starter any screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:listanime/screens/login.dart';
-import 'package:listanime/screens/register.dart';
+import './screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RegisterScreen(),
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/top_anime.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TopAnime extends StatefulWidget {
+  const TopAnime({super.key});
+
+  @override
+  State<TopAnime> createState() => _TopAnimeState();
+}
+
+class _TopAnimeState extends State<TopAnime> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
